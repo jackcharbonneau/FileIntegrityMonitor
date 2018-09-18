4c1f160bbedde5c96f67b656ce744e5d5b0a98c0
JackCharb <jlcharbonneau@wpi.edu>
2018-09-18 16:47:28 -0400

    outline with pseudocode

---
+++ fim.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# Jack Charbonneau 09/17/2018
+
+
+def main():
+    #  TODO parse arguments
+    #  TODO load in database/file
+    #  TODO for each file
+        #  TODO get checksum
+        #  TODO compare checksums
+        #  TODO if valid report valid and update datetime
+        #  TODO if invalid report invalid state last valid datetime
+    pass
+
+
+if __name__ == "__main__":
+    main()
