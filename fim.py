863ea6be4fb6686a8480b3290db2e8dc8c1622ef
JackCharb <jlcharbonneau@wpi.edu>
2018-09-18 16:56:59 -0400

    load in file of valid checksums

--- fim.py
+++ fim.py
@@ -2,16 +2,33 @@
 
 # Jack Charbonneau 09/17/2018
 
+import csv
+
 
 def main():
     #  TODO parse arguments
-    #  TODO load in database/file
+    parse()
+
+    valids = loadChecksums()
+
     #  TODO for each file
         #  TODO get checksum
         #  TODO compare checksums
         #  TODO if valid report valid and update datetime
         #  TODO if invalid report invalid state last valid datetime
     pass
+
+
+def parse():
+    pass
+
+
+def loadChecksums():
+    #  Load in database/file
+    with open("valid_checksums.csv", newline='') as f:
+        reader = csv.reader(f)
+        valids = list(reader)
+        return valids
 
 
 if __name__ == "__main__":
